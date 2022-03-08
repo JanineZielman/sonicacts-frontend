7f194e8597f13ba047f2a86677346a51001e3197
JanineZielman <janine.zielman@icloud.com>
2022-03-08 12:47:09 +0100

    news overview styling

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Link from "next/link"
+import Moment from 'moment';
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import Image from "../../components/image"
@@ -11,13 +12,29 @@ const News = ({ menus, global, page, items }) => {
   return (
     <Layout page={page} menus={menus} global={global}>
       <div className="discover">
+        <div className="highlight">
+          <div className="image">
+            <Image image={items[0].attributes.cover_image?.data?.attributes}/>
+          </div>
+          <div className="text">
+            <div className="date">{Moment(items[0].attributes.publishedAt).format('D MMM y')}</div>
+            <Link href={page.attributes.slug+'/'+items[0].attributes.slug}>
+              <a>{items[0].attributes.title}</a>
+            </Link>
+          </div>
+        </div>
+        <div className="filter">
+          <div><span>Sort By</span></div>
+          
+        </div>
         <div className="discover-container">
-          {items.map((item, i) => {
+          {items.slice(1).map((item, i) => {
             return (
               <div className="discover-item">
                 <div className="image">
                   <Image image={item.attributes.cover_image?.data?.attributes} layout='fill' objectFit='cover'/>
                 </div>
+                <div className="date">{Moment(item.attributes.publishedAt).format('D MMM y')}</div>
                 <Link href={page.attributes.slug+'/'+item.attributes.slug} key={'link'+i}>
                   <a>{item.attributes.title}</a>
                 </Link>
