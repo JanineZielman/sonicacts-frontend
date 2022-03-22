bc420483caffeaa1e28b428d5903ecb59f664e4c
JanineZielman <janine.zielman@icloud.com>
2022-03-22 15:01:24 +0100

    font animation

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
             src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"
           />
           <script src="https://cdnjs.cloudflare.com/ajax/libs/processing.js/1.6.6/processing.min.js"></script>
+          <script src="https://rawgit.com/bradley/Blotter/master/build/blotter.min.js"></script>
+          <script src="https://rawcdn.githack.com/bradley/Blotter/0e1236569790e536bd0dc6cf5555ed8e7d0e89e4/build/materials/rollingDistortMaterial.js"></script>
+          
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useRef, useState} from "react"
 import Link from "next/link"
 import Collapsible from 'react-collapsible';
 
@@ -7,14 +7,35 @@ import Seo from "../components/seo"
 import Image from "../components/image"
 import { fetchAPI } from "../lib/api"
 
+
 const Home = ({ homepage, menus, global }) => {
-  console.log(menus)
+  // console.log(menus)
+
+    const [counter, setCounter] = useState(360);
+    useEffect(() => {
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter]);
+
+    
+  
   return (
     <Layout page={homepage} menus={menus}>
       <div className="columns">
         <div className="wrapper-medium">
-          <div className="image">
-            <Image image={homepage.attributes.logo.data.attributes}/>
+          <div className="image logo">
+            <div class="glitch" data-text="Sonic Acts" style={{'--random': (Math.floor(Math.random() * 10) * 1) }}>Sonic</div> 
+            <div class="glitch" data-text="Acts" style={{'--random': (Math.floor(Math.random() * 10) * 1) }}>Acts</div> 
+            {/* <h1>
+              {('Sonic Acts').split("").map(function(char, index){
+                return (
+                  <>
+                  <div aria-hidden="true" className="glitch" style={{'--delay': (Math.floor(Math.random() * 10) * 1) + 's' }} key={index} data-text={char}>{char}</div>
+                  </>
+                );
+              })}
+            </h1> */}
+            
+            {/* <Image image={homepage.attributes.logo.data.attributes}/> */}
           </div>
           <div className="intro-text">
             <h1>{homepage.attributes.IntroText}</h1>
