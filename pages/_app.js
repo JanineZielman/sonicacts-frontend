20728f7878341e3e37a40c00c30e2fd6c164a08e
JanineZielman <janine.zielman@icloud.com>
2022-04-28 21:50:02 +0200

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,6 @@ const MyApp = ({ Component, pageProps }) => {
         /> */}
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-        <script src="https://npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js"/>
       </Head>
       <Component {...pageProps} />
       {/* <GlobalContext.Provider value={global.attributes}>
