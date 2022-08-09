0771d50b4aede70360a3d9c2f59fee2de59f750c
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-08-09 22:07:37 +0200

    visit page

--- pages/biennial/[slug]/visit/index.js
+++ pages/biennial/[slug]/visit/index.js
@@ -4,21 +4,65 @@ import Layout from "../../../../components/layout"
 import { fetchAPI } from "../../../../lib/api"
 
 
-const Visit = ({ menus, global, params }) => {
+const Visit = ({ menus, global, params, festival }) => {
 	const page = {
     attributes:
       	{slug: `biennial/${params.slug}/visit`}
 	}
   return (
-    <Layout page={page} menus={menus} global={global}>
-      
-    </Layout>
+    <section className="festival-wrapper">
+      <Layout page={page} menus={menus} global={global}>
+        <section className="article visit">
+          <div className="content">
+            <div className="wrapper">
+              {festival.map((item, i) => {
+                return(
+                  <>
+                  {item.text &&
+                  <div className="text-block">
+                    <ReactMarkdown 
+                      children={item.text} 
+                    />
+                  </div>
+                  }
+                </>
+                )
+              })}
+            </div>
+            <div className="sidebar">
+              {festival.map((item, i) => {
+                return(
+                  <>
+                  {item.locations &&
+                  <>
+                    <span>Locations:</span>
+                    {item.locations.data.map((loc, j) => {
+                      return(
+                        <div className="locations">
+                          {loc.attributes.title} 
+                          <span>{loc.attributes.location}</span>
+                          
+                        </div>
+                        
+                      )
+                    })}
+                  </>
+                  }
+                </>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      </Layout>
+    </section>
   )
 }
 
 export async function getServerSideProps({params}) {
   // Run API calls in parallel
-  const [globalRes, menusRes] = await Promise.all([
+  const [festivalRes, globalRes, menusRes] = await Promise.all([
+    fetchAPI(`/biennials?filters[slug][$eq]=${params.slug}&populate[visit][populate]=*`),
     fetchAPI("/global", { populate: "*" }),
     fetchAPI("/menus", { populate: "*" }),
   ])
@@ -26,6 +70,7 @@ export async function getServerSideProps({params}) {
 
   return {
     props: {
+      festival: festivalRes.data[0].attributes.visit,
       global: globalRes.data,
       menus: menusRes.data,
 			params: params,
