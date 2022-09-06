27a106b3a6a22f5b9bf30fc0b473790691e40743
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-09-06 10:58:37 +0200

    poster file and clean up

---
+++ pages/biennial/poster.js
@@ -0,0 +1,28 @@
+const Poster = () => {
+	const text = "Sonic  Acts Bien-_nial 20 22  Son ic"
+	return(
+		<section className="festival biennial-poster">
+			<div className="festival-hero-bg type">
+				<div className={`title`}>
+					<div className={`layer1`}>
+						{(text).split("").map(function(char, index){
+						return <span className={`letter`} aria-hidden="true" key={index} style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>{char}</span>;
+						})}
+					</div>
+					<div className={`layer2`}>
+						{(text).split("").map(function(char, index){
+						return <span className={`letter`} aria-hidden="true" key={index} style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>{char}</span>;
+						})}
+					</div>
+					<div className={`layer3`}>
+						{(text).split("").map(function(char, index){
+						return <span className={`letter`} aria-hidden="true" key={index} style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>{char}</span>;
+						})}
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Poster
