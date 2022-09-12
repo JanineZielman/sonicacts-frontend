556a7e961d339325b4303b28d2226acd2fbe86c5
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-09-12 13:14:13 +0200

    date fix

--- components/biennial-article.js
+++ components/biennial-article.js
@@ -129,7 +129,7 @@ const Article = ({page, relations, params, programmes}) => {
 									})}
 								</div>
 								<span>When</span>
-								<div className="date">{Moment(programmes?.start_date).format('D MMM y')} {programmes?.end_date && <> – {programmes?.end_date} </>}</div>
+								<div className="date">{Moment(programmes?.start_date).format('D MMM')} {programmes?.end_date && <> – {Moment(programmes?.end_date).format('D MMM')} </>}</div>
 								<span>Time</span>
 								<div className="date">{programmes?.start_time?.substring(0, 5)} – {programmes?.end_time?.substring(0, 5)}</div>
 								<br/>
