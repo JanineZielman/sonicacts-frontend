b7d71c83b2babae051a372d632c0f303c1709792
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-11-23 18:21:59 +0100

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,20 @@ class MyDocument extends Document {
           />   
           <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"/>
           <script async src="//npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js"/>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MQ9CG4C80G"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-MQ9CG4C80G', { 'anonymize_ip': true });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
