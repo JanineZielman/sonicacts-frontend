7955cdd8470c1f54ecb458834da8fc2f0c02e169
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-06-02 12:35:38 +0200

    sort items on date sub pages

--- pages/discover/[slug]/index.js
+++ pages/discover/[slug]/index.js
@@ -94,6 +94,10 @@ export async function getServerSideProps({params, preview = null}) {
   const agendaItems = await fetchAPI(`/agenda-items?sort[0]=date%3Adesc&filters[category][slug][$eq]=${params.slug}&populate=*`);
 
   var mergedItems = itemsRel.data.concat(agendaItems.data)
+  
+  mergedItems.sort(function(a,b){
+    return new Date(b.attributes.date) - new Date(a.attributes.date);
+  });
 
   const [menusRes, globalRes] = await Promise.all([
     fetchAPI("/menus", { populate: "*" }),
