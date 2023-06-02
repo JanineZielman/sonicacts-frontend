a94a6c460baabc4866a9c7dc300ce89af6e9d581
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-06-02 12:14:55 +0200

    small adjustments sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 const siteUrl = 'https://sonicacts.com/';
 module.exports = {
   siteUrl,
-  exclude: ["/404"],
+  exclude: ["/404", "/biennial/hero", "/biennial/poster", "/biennial/type", "/biennial/type0", "/biennial/type1", "/biennial/type2", "/logo"],
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
