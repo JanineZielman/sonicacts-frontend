464a37a3a88b34fea1256cefe63c5c6acff80d41
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-09-14 12:01:10 +0200

    preview try 1

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,3 +1,23 @@
+// import { getPageData } from "../../lib/api"
+
+// export default async function handler(req, res) {
+// 	if (req.query.secret !== (process.env.STRAPI_PREVIEW_SECRET || 'secret-token')) {
+// 		return res.status(401).json({ message: req.query.type.replace('-item', '') });
+// 	}
+
+// 	const pageData = await getPageData(req.query.slug, true, req.query.type);
+
+// 	if (!pageData) {
+// 		return res.status(401).json({ message: "Invalid slug" });
+// 	}
+
+// 	res.setPreviewData({});
+
+// 	res.writeHead(307, { Location: `/${req.query.type.replace('-item', '')}/${req.query.slug}` });
+// 	res.end();
+// };
+
+
 import { getPageData } from "../../lib/api"
 
 export default async function handler(req, res) {
@@ -16,3 +36,4 @@ export default async function handler(req, res) {
 	res.writeHead(307, { Location: `/${req.query.type.replace('-item', '')}/${req.query.slug}` });
 	res.end();
 };
+
