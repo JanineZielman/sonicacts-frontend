e65e91fc784124295e90ff6979355e8452dfe35b
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-08-20 20:39:44 +0200

    media curator io

---
+++ pages/archive/filter/media.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from "react"
+import Layout from "../../../components/layout"
+import { fetchAPI } from "../../../lib/api"
+
+const DiscoverFiltered = ({ menus, global, page, categories}) => {
+  let filter = "media"
+
+
+  useEffect(() => {
+
+    var loader=new function(){this.rC=-1,this.r=[],this.add=function(t){this.r.push(t)},this.addTag=function(t,e){var i=document.getElementsByTagName("head")[0],s=t.indexOf(".js")>0?"script":"link",n=document.createElement(s);i.appendChild(n),n.onload=e,n.charset="UTF-8","script"===s?(n.type="text/javascript",n.src=t):"link"===s&&(n.rel="stylesheet",n.href=t)},this.loadNext=function(){if(this.rC++,this.rC>=this.r.length)this.done();else{var t=this.r[this.rC];this.addTag(t,this.loadNext.bind(this))}},this.done=function(){this.onResourcesLoaded(window.Curator)},this.load=function(t){this.onResourcesLoaded=t,this.loadNext()}};
+
+    // Config
+    var config = {"type":"Tetris","post":{"template":"post-tetris","imageHeight":"100%","minWidth":280,"maxLines":3,"showTitles":true,"showShare":true,"showComments":false,"showLikes":false,"autoPlayVideos":false,"clickAction":"open-popup","clickReadMoreAction":"open-popup"},"widget":{"showLoadMore":true,"postsPerPage":25,"verticalSpacing":20,"horizontalSpacing":20,"autoLoadNew":false,"lazyLoadType":"none","gridMobile":false},"lang":"en","container":"#curator-feed-default-feed-layout","debug":0,"hidePoweredBy":false,"embedSource":"","forceHttps":false,"feed":{"id":"5e5a781d-0dba-4966-823a-29c0591ac51e","apiEndpoint":"https:\/\/api.curator.io","postsPerPage":12,"params":{},"limit":25},"popup":{"template":"popup","templateWrapper":"popup-wrapper","autoPlayVideos":false,"deepLink":false},"filter":{"template":"filter","showNetworks":false,"showSources":false,"showAll":false,"default":"all","limitPosts":false,"limitPostNumber":0,"period":""},"theme":"sydney"};
+    var colours = {"widgetBgColor":"transparent","bgColor":"#ffffff","borderColor":"#cccccc","iconColor":"#222222","textColor":"#222222","linkColor":"#999999","dateColor":"#000000","footerColor":"#ffffff","tabIndexColor":"#cccccc"};
+    var styles = {};
+
+    // Bootstrap
+    function loaderCallback () {
+        window.Curator.loadWidget(config, colours, styles);
+    }
+
+    // Run Loader
+    loader.add('https://cdn.curator.io/5.0/curator.embed.css');
+    loader.add('https://cdn.curator.io/published-css/5e5a781d-0dba-4966-823a-29c0591ac51e.css');
+
+    loader.add('https://cdn.curator.io/5.0/curator.embed.js');
+
+    
+
+    loader.load(loaderCallback);
+
+  })
+
+  return (
+    <Layout page={page} menus={menus} global={global}>
+      <div className="discover">
+        <div className="filter">
+          <div><span>Filter by category</span></div>
+						<a key={'category-all'} href={`/archive`}>All</a>
+						{categories?.map((category, i) => {
+							return (
+								<a key={'category'+i} href={`/archive/filter/${category?.attributes.slug}`}
+									className={category?.attributes.slug == filter && 'active'}
+								>
+									{category?.attributes.title}
+								</a>
+							)
+						})}
+        </div>
+        <div className="discover-container">
+          <div id="curator-feed-default-feed-layout">
+          </div>
+        </div>
+      </div>
+      
+    </Layout>
+  )
+}
+
+export async function getServerSideProps() {
+  const [pageRes, categoryRes, globalRes, menusRes] = await Promise.all([
+    fetchAPI("/discover-overview", { populate: "*" }),
+    fetchAPI("/categories?sort[0]=order&filters[$or][0][sub_category][$null]=true&filters[$or][1][sub_category][$eq]=false&populate=*"),
+    fetchAPI("/global?populate[prefooter][populate]=*&populate[socials][populate]=*&populate[image][populate]=*&populate[footer_links][populate]=*&populate[favicon][populate]=*", { populate: "*" }),
+    fetchAPI("/menus", { populate: "*" }),
+  ])
+
+  return {
+    props: {
+      page: pageRes.data,
+      categories: categoryRes.data,
+      global: globalRes.data,
+      menus: menusRes.data,
+    },
+  };
+}
+
+export default DiscoverFiltered
