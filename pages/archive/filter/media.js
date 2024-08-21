598389e800692ea5dd1752162204c6067005f575
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-08-21 10:31:51 +0200

    update media

--- pages/archive/filter/media.js
+++ pages/archive/filter/media.js
@@ -7,28 +7,30 @@ const DiscoverFiltered = ({ menus, global, page, categories}) => {
 
 
   useEffect(() => {
+    setTimeout(() => {
+      
+      var loader=new function(){this.rC=-1,this.r=[],this.add=function(t){this.r.push(t)},this.addTag=function(t,e){var i=document.getElementsByTagName("head")[0],s=t.indexOf(".js")>0?"script":"link",n=document.createElement(s);i.appendChild(n),n.onload=e,n.charset="UTF-8","script"===s?(n.type="text/javascript",n.src=t):"link"===s&&(n.rel="stylesheet",n.href=t)},this.loadNext=function(){if(this.rC++,this.rC>=this.r.length)this.done();else{var t=this.r[this.rC];this.addTag(t,this.loadNext.bind(this))}},this.done=function(){this.onResourcesLoaded(window.Curator)},this.load=function(t){this.onResourcesLoaded=t,this.loadNext()}};
 
-    var loader=new function(){this.rC=-1,this.r=[],this.add=function(t){this.r.push(t)},this.addTag=function(t,e){var i=document.getElementsByTagName("head")[0],s=t.indexOf(".js")>0?"script":"link",n=document.createElement(s);i.appendChild(n),n.onload=e,n.charset="UTF-8","script"===s?(n.type="text/javascript",n.src=t):"link"===s&&(n.rel="stylesheet",n.href=t)},this.loadNext=function(){if(this.rC++,this.rC>=this.r.length)this.done();else{var t=this.r[this.rC];this.addTag(t,this.loadNext.bind(this))}},this.done=function(){this.onResourcesLoaded(window.Curator)},this.load=function(t){this.onResourcesLoaded=t,this.loadNext()}};
+      // Config
+      var config = {"type":"Tetris","post":{"template":"post-tetris","imageHeight":"100%","minWidth":280,"maxLines":3,"showTitles":true,"showShare":true,"showComments":false,"showLikes":false,"autoPlayVideos":false,"clickAction":"open-popup","clickReadMoreAction":"open-popup"},"widget":{"showLoadMore":true,"postsPerPage":25,"verticalSpacing":20,"horizontalSpacing":20,"autoLoadNew":false,"lazyLoadType":"none","gridMobile":false},"lang":"en","container":"#curator-feed-default-feed-layout","debug":0,"hidePoweredBy":false,"embedSource":"","forceHttps":false,"feed":{"id":"5e5a781d-0dba-4966-823a-29c0591ac51e","apiEndpoint":"https:\/\/api.curator.io","postsPerPage":12,"params":{},"limit":25},"popup":{"template":"popup","templateWrapper":"popup-wrapper","autoPlayVideos":false,"deepLink":false},"filter":{"template":"filter","showNetworks":false,"showSources":false,"showAll":false,"default":"all","limitPosts":false,"limitPostNumber":0,"period":""},"theme":"sydney"};
+      var colours = {"widgetBgColor":"transparent","bgColor":"#ffffff","borderColor":"#cccccc","iconColor":"#222222","textColor":"#222222","linkColor":"#999999","dateColor":"#000000","footerColor":"#ffffff","tabIndexColor":"#cccccc"};
+      var styles = {};
 
-    // Config
-    var config = {"type":"Tetris","post":{"template":"post-tetris","imageHeight":"100%","minWidth":280,"maxLines":3,"showTitles":true,"showShare":true,"showComments":false,"showLikes":false,"autoPlayVideos":false,"clickAction":"open-popup","clickReadMoreAction":"open-popup"},"widget":{"showLoadMore":true,"postsPerPage":25,"verticalSpacing":20,"horizontalSpacing":20,"autoLoadNew":false,"lazyLoadType":"none","gridMobile":false},"lang":"en","container":"#curator-feed-default-feed-layout","debug":0,"hidePoweredBy":false,"embedSource":"","forceHttps":false,"feed":{"id":"5e5a781d-0dba-4966-823a-29c0591ac51e","apiEndpoint":"https:\/\/api.curator.io","postsPerPage":12,"params":{},"limit":25},"popup":{"template":"popup","templateWrapper":"popup-wrapper","autoPlayVideos":false,"deepLink":false},"filter":{"template":"filter","showNetworks":false,"showSources":false,"showAll":false,"default":"all","limitPosts":false,"limitPostNumber":0,"period":""},"theme":"sydney"};
-    var colours = {"widgetBgColor":"transparent","bgColor":"#ffffff","borderColor":"#cccccc","iconColor":"#222222","textColor":"#222222","linkColor":"#999999","dateColor":"#000000","footerColor":"#ffffff","tabIndexColor":"#cccccc"};
-    var styles = {};
+      // Bootstrap
+      function loaderCallback () {
+          window.Curator?.loadWidget(config, colours, styles);
+      }
 
-    // Bootstrap
-    function loaderCallback () {
-        window.Curator.loadWidget(config, colours, styles);
-    }
+      // Run Loader
+      loader.add('https://cdn.curator.io/5.0/curator.embed.css');
+      loader.add('https://cdn.curator.io/published-css/5e5a781d-0dba-4966-823a-29c0591ac51e.css');
 
-    // Run Loader
-    loader.add('https://cdn.curator.io/5.0/curator.embed.css');
-    loader.add('https://cdn.curator.io/published-css/5e5a781d-0dba-4966-823a-29c0591ac51e.css');
+      loader.add('https://cdn.curator.io/published/5e5a781d-0dba-4966-823a-29c0591ac51e.js'); 
 
-    loader.add('https://cdn.curator.io/5.0/curator.embed.js');
-
-    
-
-    loader.load(loaderCallback);
+      
+      
+      loader.load(loaderCallback);
+    }, 200);
 
   })
 
