fadfba6aa0cf341520b3854cdb56f15835cc9b37
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-10-01 20:35:27 +0200

    fix date homepage

--- pages/index.js
+++ pages/index.js
@@ -207,13 +207,54 @@ const Home = ({ homepage, menus, global, items, about }) => {
                                     }
                                   </div>
                                 )}
-                                {item.attributes.date && (
+                                {/* {item.attributes.date && (
                                   <span>
                                     {Moment(item.attributes.date).format(
                                       "D MMM y"
                                     )}
                                   </span>
-                                )}
+                                )} */}
+                                {item.attributes.date &&
+                                  <>
+                                    {item.attributes.dates?.[0] ?
+                                      <span>
+                                        {item.attributes.dates.map((date, i) => {
+                                          return(
+                                            <span className={`date ${i}`} key={`dates-${i}`}>
+                                              {date.single_date &&
+                                                <>
+                                                {i == 0 && Moment(item.attributes.date).format('D MMM y')}
+                                                , {Moment(date.single_date).format('D MMM y')}
+                                                </>
+                                              }
+                                              {date.end_date &&
+                                                <>
+                                                  {(Moment(item.attributes.date).format('y') == Moment(date.end_date).format('y')) ? 
+                                                    <>
+                                                      {(Moment(item.attributes.date).format('MMM y') == Moment(date.end_date).format('MMM y')) ?
+                                                        <>{Moment(item.attributes.date).format('D')}&nbsp;– {Moment(date.end_date).format('D MMM y')}</>
+                                                      :
+                                                        <>{Moment(item.attributes.date).format('D MMM')}&nbsp;– {Moment(date.end_date).format('D MMM y')}</>
+                                                      }
+                                                    </>
+                                                    :
+                                                    <>
+                                                      {Moment(item.attributes.date).format('D MMM y')}&nbsp;– {Moment(date.end_date).format('D MMM y')}
+                                                    </>
+                                                  }
+                                                </>
+                                              }
+                                            </span>
+                                          )
+                                        })}
+                                      </span>
+                                    : 
+                                    <span>
+                                      {Moment(item.attributes.date).format('D MMM y')}
+                                    </span>
+                                    }
+                                  </>
+                                }
                                 {item.attributes.title && (
                                   <h2>{item.attributes.title}</h2>
                                 )}
