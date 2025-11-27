52e5b01a12c06c5f78dbbae3150088987332e631
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-11-27 01:23:21 +0100

    portal fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,104 +1,98 @@
-import App from "next/app";
-import Head from "next/head";
+import App from "next/app"
+import Head from "next/head"
+import "../assets/css/style.scss"
+import "../assets/css/article.scss"
+import "../assets/css/filter.scss"
+import "../assets/css/agenda.scss"
+import "../assets/css/search.scss"
+import "../assets/css/slider.scss"
+import "../assets/css/festival.scss"
+import "../assets/css/animation.scss"
+import "../assets/css/timetable.scss"
+import "../assets/css/error.scss"
+import "../assets/css/breakpoints.scss"
+import "../assets/css/festival-breakpoints.scss"
+import { createContext } from "react"
+import { fetchAPI } from "../lib/api"
+import { getStrapiMedia } from "../lib/media"
+import React, { useEffect, useState } from "react"
 import { useRouter } from "next/router";
-import { createContext, useEffect, useState } from "react";
 
-import { fetchAPI } from "../lib/api";
-import { getStrapiMedia } from "../lib/media";
-
-// Global context
-export const GlobalContext = createContext({});
+// Store Strapi Global object in context
+export const GlobalContext = createContext({})
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
-  const { global } = pageProps;
+  const isBiennial2026 = router.pathname.startsWith("/biennial/biennial-2026");
+  const { global } = pageProps
   const [loading, setLoading] = useState(true);
 
-  // Detect Biennial route
-  const isBiennial2026 = router.pathname.startsWith("/biennial/biennial-2026");
-
-  // Dynamically remove previously loaded dynamic CSS on route change
   useEffect(() => {
-    const oldStyles = document.querySelectorAll("link[data-dynamic]");
-    oldStyles.forEach((el) => el.remove());
-  }, [router.pathname]);
-
-  // Set loading state (optional)
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 100);
-    return () => clearTimeout(timer);
+    setTimeout(function () {
+      setLoading(false)
+    }, 100);
   }, []);
 
-  if (!isBiennial2026) {
-    // PORTAL STYLES
-    import("../assets/css/style.scss");
-    import("../assets/css/article.scss");
-    import("../assets/css/filter.scss");
-    import("../assets/css/agenda.scss");
-    import("../assets/css/search.scss");
-    import("../assets/css/slider.scss");
-    import("../assets/css/festival.scss");
-    import("../assets/css/animation.scss");
-    import("../assets/css/timetable.scss");
-    import("../assets/css/error.scss");
-    import("../assets/css/breakpoints.scss");
-    import("../assets/css/festival-breakpoints.scss");
-  }
-
   return (
-    <> <Head>
-      {/* Global favicon */} <link
-        rel="shortcut icon"
-        href={getStrapiMedia(global?.attributes?.favicon?.data?.attributes)}
-      />
-
-      {/* Portal Slick Carousel CSS */}
-      {!isBiennial2026 && (
-        <>
-          <link
-            data-dynamic
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            data-dynamic
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-        </>
-      )}
-
-      {/* Biennial CSS loaded dynamically */}
-      {isBiennial2026 && (
+    <>
+      <Head>
         <link
-          rel="stylesheet"
-          href="/biennial.css"
-          data-dynamic
+          rel="shortcut icon"
+          href={getStrapiMedia(global.attributes.favicon?.data?.attributes)}
         />
-      )}
-    </Head>
+        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 
-      <GlobalContext.Provider value={global?.attributes || {}}>
-        {/* Optional loader */}
-        {/* {loading ? <div className="loader" /> : <Component {...pageProps} />} */}
+        {!isBiennial2026 && (
+          <>
+            <link rel="stylesheet" href="/assets/css/style.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/article.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/filter.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/agenda.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/search.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/slider.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/festival.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/animation.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/timetable.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/error.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/breakpoints.css" data-dynamic />
+            <link rel="stylesheet" href="/assets/css/festival-breakpoints.css" data-dynamic />
+          </>
+        )}
+
+        {isBiennial2026 && (
+          <link
+            rel="stylesheet"
+            href="/biennial.css"
+            data-dynamic
+          />
+        )}
+      </Head>
+      <GlobalContext.Provider value={global.attributes}>
+        {/* {loading ?
+          <div className="loader"></div>
+          :
+          <>
+            <div className={`loader ${loading}`}></div>
+            <Component {...pageProps} />
+          </>
+        } */}
         <Component {...pageProps} />
+
       </GlobalContext.Provider>
     </>
+  )
+}
 
-  );
-};
 
-// Fetch global settings
 MyApp.getInitialProps = async (ctx) => {
-  const appProps = await App.getInitialProps(ctx);
-  const globalRes = await fetchAPI("/global", { populate: "*" });
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(ctx)
+  // // Fetch global site settings from Strapi
+  const globalRes = await fetchAPI("/global", { populate: "*" })
+  // Pass the data to our page via props
+  return { ...appProps, pageProps: { global: globalRes.data } }
+  // return { ...appProps }
+}
 
-  return {
-    ...appProps,
-    pageProps: { ...appProps.pageProps, global: globalRes.data },
-  };
-};
-
-export default MyApp;
+export default MyApp
