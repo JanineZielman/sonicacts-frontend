e419eec1ada1e98c0ddb47cd47d6e8dd7bd8474a
Christoph Knoth <github@christoph-knoth.com>
2026-01-05 14:28:56 +0100

    mobile adjustments 02

--- pages/_document.js
+++ pages/_document.js
@@ -85,6 +85,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
+          />
           <link
             rel="icon"
             type="image/png"
@@ -108,7 +112,6 @@ export default class MyDocument extends Document {
           {!isBiennial2026 && (
             <>
           <link rel="canonical" href="https://sonicacts.com/" />
-          <meta name="theme-color" content="transparent" />
 
               <script
                 async
