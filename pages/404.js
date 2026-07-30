7e7a28447ab319826e95bfcf12e74221adf55d5d
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2026-07-30 10:58:31 +0200

    fix

--- pages/404.js
+++ pages/404.js
@@ -1,52 +1,103 @@
 import { fetchAPI } from "../lib/api"
 import Layout from "../components/new-layout"
 
-const FourOhFour = ({menus, global}) => {
-	const page = {
-		attributes: {
-			slug: '404'
-		}
-	}
+const FourOhFour = ({ menus, global }) => {
+  const page = {
+    attributes: {
+      slug: "404",
+    },
+  }
 
   const festival = {
-		attributes: {
-			slug: '404'
-		}
-	}
-	
+    attributes: {
+      slug: "404",
+    },
+  }
+
+  const errorContent = (
+    <section className="error">
+      <p>Sorry, we couldn't find this page...</p>
+
+      <a href="/">
+        <img
+          className="arrow"
+          src="/arrow.svg"
+          alt=""
+        />
+        Homepage
+      </a>
+
+      <div className="error-animation">
+        <span
+          data-text="4"
+          className="glitch"
+          style={{ "--delay": "0.8s" }}
+        >
+          4
+        </span>
+
+        <span
+          data-text="0"
+          className="glitch"
+          style={{ "--delay": "3.2s" }}
+        >
+          0
+        </span>
+
+        <span
+          data-text="4"
+          className="glitch"
+          style={{ "--delay": "5.6s" }}
+        >
+          4
+        </span>
+      </div>
+    </section>
+  )
+
+  // Ensure the 404 page still renders when Strapi is unavailable.
+  if (!global) {
+    return errorContent
+  }
+
   return (
-    <Layout menus={menus} page={page} global={global} festival={festival}>
-			<section className="error">
-				<p>Sorry, we couldn't find this page...</p>
-				<a href="/">
-					<img className="arrow" src="/arrow.svg"/> Homepage
-				</a>
-        <div className="error-animation">
-          <span  data-text="4" className="glitch" style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>4</span>
-          <span  data-text="0" className="glitch" style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>0</span>
-          <span  data-text="4" className="glitch" style={{'--delay': (Math.floor(Math.random() * 10) * 0.8) + 's' }}>4</span>
-        </div>
-			</section>
+    <Layout
+      menus={menus}
+      page={page}
+      global={global}
+      festival={festival}
+    >
+      {errorContent}
     </Layout>
   )
 }
 
-
 export async function getStaticProps() {
+  try {
+    const [globalRes, menusRes] = await Promise.all([
+      fetchAPI(
+        "/global?populate[prefooter][populate]=*&populate[socials][populate]=*&populate[image][populate]=*&populate[footer_links][populate]=*&populate[favicon][populate]=*",
+        { populate: "*" }
+      ),
+      fetchAPI("/menus", { populate: "*" }),
+    ])
 
-  const [globalRes, menusRes] = await Promise.all([
-    fetchAPI("/global?populate[prefooter][populate]=*&populate[socials][populate]=*&populate[image][populate]=*&populate[footer_links][populate]=*&populate[favicon][populate]=*", { populate: "*" }),
-    fetchAPI("/menus", { populate: "*" }),
-  ])
+    return {
+      props: {
+        global: globalRes?.data ?? null,
+        menus: menusRes?.data ?? [],
+      },
+    }
+  } catch (error) {
+    console.error("Could not load Strapi data for the 404 page:", error)
 
-  return {
-    props: { 
-      global: globalRes.data,
-      menus: menusRes.data
-    },
-    revalidate: 1,
-  };
+    return {
+      props: {
+        global: null,
+        menus: [],
+      },
+    }
+  }
 }
-
 
 export default FourOhFour
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import App from "next/app"
 import Head from "next/head"
+import { createContext } from "react"
+
 import "../assets/css/style.scss"
 import "../assets/css/article.scss"
 import "../assets/css/filter.scss"
@@ -12,59 +14,91 @@ import "../assets/css/timetable.scss"
 import "../assets/css/error.scss"
 import "../assets/css/breakpoints.scss"
 import "../assets/css/festival-breakpoints.scss"
-import { createContext } from "react"
+
 import { fetchAPI } from "../lib/api"
 import { getStrapiMedia } from "../lib/media"
-import React, { useEffect, useState } from "react"
 
-// Store Strapi Global object in context
 export const GlobalContext = createContext({})
 
 const MyApp = ({ Component, pageProps }) => {
-  const { global } = pageProps
-  const [loading, setLoading] = useState(true);
+  const global = pageProps?.global ?? {
+    attributes: {},
+  }
 
-  useEffect(() => {
-    setTimeout(function () {
-      setLoading(false)
-    }, 100);
-  }, []);
+  const globalAttributes = global?.attributes ?? {}
+  const favicon = globalAttributes?.favicon?.data?.attributes
+  const faviconUrl = favicon ? getStrapiMedia(favicon) : null
 
   return (
     <>
       <Head>
-        <link
-          rel="shortcut icon"
-          href={getStrapiMedia(global.attributes.favicon?.data?.attributes)}
-        />
-        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      </Head>
-      <GlobalContext.Provider value={global.attributes}>
-        {/* {loading ?
-          <div className="loader"></div>
-          :
-          <>
-            <div className={`loader ${loading}`}></div>
-            <Component {...pageProps} />
-          </>
-        } */}
-        <Component {...pageProps} />
+        {faviconUrl && (
+          <link
+            rel="shortcut icon"
+            href={faviconUrl}
+          />
+        )}
 
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </Head>
+
+      <GlobalContext.Provider value={globalAttributes}>
+        <Component {...pageProps} />
       </GlobalContext.Provider>
     </>
   )
 }
 
+MyApp.getInitialProps = async (appContext) => {
+  // Collect props from the current page.
+  const appProps = await App.getInitialProps(appContext)
 
-MyApp.getInitialProps = async (ctx) => {
-  // Calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(ctx)
-  // // Fetch global site settings from Strapi
-  const globalRes = await fetchAPI("/global", { populate: "*" })
-  // Pass the data to our page via props
-  return { ...appProps, pageProps: { global: globalRes.data } }
-  // return { ...appProps }
+  try {
+    const globalRes = await fetchAPI("/global", {
+      populate: "*",
+    })
+
+    return {
+      ...appProps,
+      pageProps: {
+        // Preserve props returned by getStaticProps/getServerSideProps.
+        ...appProps.pageProps,
+
+        // Prefer a global value fetched by the page itself.
+        global:
+          appProps.pageProps?.global ??
+          globalRes?.data ?? {
+            attributes: {},
+          },
+      },
+    }
+  } catch (error) {
+    console.error("Could not load global Strapi data in _app.js:", error)
+
+    return {
+      ...appProps,
+      pageProps: {
+        ...appProps.pageProps,
+
+        // Preserve the page's global data or use a safe fallback.
+        global:
+          appProps.pageProps?.global ?? {
+            attributes: {},
+          },
+      },
+    }
+  }
 }
 
 export default MyApp
