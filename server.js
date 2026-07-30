b97f61a938d822af4887b1371b386f1dfb1e517d
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2026-07-30 11:01:59 +0200

    port 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const port = parseInt(process.env.PORT || '3001', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -14,8 +14,7 @@ app.prepare().then(() => {
   }).listen(port)
 
   console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
+    `> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
     }`
   )
 })
